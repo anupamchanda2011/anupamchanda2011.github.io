bccf65b34a3d8b56e3a9b82a095db181613b1752
Anupam Chandra <anupamchanda2011@gmail.com>
2021-07-17 03:28:46 +0530

    constant.js file created

---
+++ constant.js
@@ -0,0 +1,21 @@
+const mfUrl = " https://api.mfapi.in/mf/";
+const mfNames = [
+	"Axis Bluechip Fund - Direct Plan - Growth",
+	"SBI Blue Chip Fund - Direct Plan - Growth",
+	"ICICI Prudential Bluechip Fund - Direct Plan - Growth"
+];
+const links = [
+	"https://www.moneycontrol.com/mutual-funds/nav/axis-bluechip-fund-direct-plan-growth/MAA181",
+ 	"https://www.moneycontrol.com/mutual-funds/nav/sbi-blue-chip-fund-direct-plan-growth/MSB532",
+ 	"https://www.moneycontrol.com/mutual-funds/nav/icici-prudential-bluechip-fund-direct-plan-growth/MPI1134"
+]
+const mfIds = [
+	"120465",
+	"119598",
+	"120586"
+];
+const navLimits = [
+	50,
+	57,
+	60
+];
