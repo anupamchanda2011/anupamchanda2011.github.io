ee3aa96d141c1542a4355028602b5e156de019ef
Anupam Chandra <anupamchanda2011@gmail.com>
2021-07-20 23:50:38 +0530

    difference column added

--- utility.js
+++ utility.js
@@ -14,11 +14,19 @@ function drawTable(table, mfNames, mfUrl, links, navLimits, mfIds, divId){
 			table = table + "<tr><td>";
 		}
 		else{
-			table = table + "<tr style='background-color:#14f514;'><td>";
+			table = table + "<tr style='background-color:#e6ffe6;'><td>";
 		}
 		table = table + mfNames[i] + "</td><td>";
 		table = table + mfDetails.data[0].nav + "</td><td>";
-		table = table + navLimits[i] + "</td><td><a href='";
+		table = table + navLimits[i] + "</td>";
+		var diff = (mfDetails.data[0].nav - mfDetails.data[1].nav).toFixed(2);
+		if(diff < 0){
+			table = table + "<td style='color:green'>";
+		}
+		else{
+			table = table + "<td style='color:red'>";
+		}
+		table = table + diff + "</td><td><a href='";
 		table = table + links[i] + "'>Links</a></td></tr>";
 	}
 	table = table + "</table>";
