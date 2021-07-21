5163878ec8bd0a417fad467acbfcce7ea9752d10
Anupam Chandra <anupamchanda2011@gmail.com>
2021-07-22 01:10:32 +0530

    unused code removed

--- constant.js
+++ constant.js
@@ -1,5 +1,29 @@
 const mutualFunds = [
 	{
+		"type" : "elss",
+		"name" : "ELSS Tax Saver",
+		"data" : [
+			{
+				"name" : "Axis Long Term Equity Fund - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/axis-long-term-equity-fund-direct-plan-growth/MAA192",
+				"amfiLink" : "https://api.mfapi.in/mf/120503",
+				"threshold" : 70
+			},
+			{
+				"name" : "Aditya Birla Sun Life Tax Relief 96 - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/aditya-birla-sun-life-tax-relief-96-direct-plan-growth/MBS847",
+				"amfiLink" : "https://api.mfapi.in/mf/119544",
+				"threshold" : 40
+			},
+			{
+				"name" : "DSP Tax Saver Fund - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/dsp-tax-saver-fund-direct-plan-growth/MDS572",
+				"amfiLink" : "https://api.mfapi.in/mf/119242",
+				"threshold" : 75
+			}
+		]
+	},
+	{
 		"type" : "largeCap",
 		"name" : "Large Cap Funds",
 		"data" : [
@@ -66,67 +90,3 @@ const mutualFunds = [
 		]
 	}
 ];
-
-
-
-
-// const mfGroups = [
-// 	"largeCaps",
-// 	"midCaps"
-// ];
-// const largeCaps = [
-// 	"Axis Bluechip Fund - Direct Plan - Growth",
-// 	"SBI Blue Chip Fund - Direct Plan - Growth",
-// 	"ICICI Prudential Bluechip Fund - Direct Plan - Growth",
-// 	"Mirae Asset Large Cap Fund - Direct Plan - Growth",
-// 	"Aditya Birla Sun Life Frontline Equity Fund - Direct Plan - Growth"
-// ];
-// const largeCapLinks = [
-// 	"https://www.moneycontrol.com/mutual-funds/nav/axis-bluechip-fund-direct-plan-growth/MAA181",
-//  	"https://www.moneycontrol.com/mutual-funds/nav/sbi-blue-chip-fund-direct-plan-growth/MSB532",
-//  	"https://www.moneycontrol.com/mutual-funds/nav/icici-prudential-bluechip-fund-direct-plan-growth/MPI1134",
-//  	"https://www.moneycontrol.com/mutual-funds/nav/mirae-asset-large-cap-fund-direct-plan-growth/MMA096",
-//  	"https://www.moneycontrol.com/mutual-funds/nav/aditya-birla-sun-life-frontline-equity-fund-direct-plan-growth/MBS813"
-// ]
-// const largeCapMFIds = [
-// 	"120465",
-// 	"119598",
-// 	"120586",
-// 	"118825",
-// 	"119528"
-// ];
-// const largeCapNAVLimits = [
-// 	50,
-// 	57,
-// 	60,
-// 	75,
-// 	330
-// ];
-// const midCaps = [
-// 	"HDFC Mid-Cap Opportunities Fund - Direct Plan - Growth"
-// ];
-// const midCapLinks = [
-//  	"https://www.moneycontrol.com/mutual-funds/nav/hdfc-mid-cap-opportunities-fund-direct-plan-growth/MHD1161"
-// ]
-// const midCapMFIds = [
-// 	"118989"
-// ];
-// const midCapNAVLimits = [
-// 	90
-// ];
-// const focusedFunds = [
-// 	"Axis Focused 25 Fund - Growth",
-// 	"SBI Focused Equity Fund - Regular Plan - Growth"
-// ];
-// const focusedFundsLinks = [
-//  	"https://www.moneycontrol.com/mutual-funds/nav/axis-focused-25-fund-growth/MAA169",
-//  	"https://www.moneycontrol.com/mutual-funds/nav/sbi-focused-equity-fund-regular-plan-growth/MSB059"
-// ]
-// const focusedFundsMFIds = [
-// 	"117560",
-// 	"102756"
-// ];
-// const focusedFundsNAVLimits = [
-// 	40,
-// 	210
-// ];
