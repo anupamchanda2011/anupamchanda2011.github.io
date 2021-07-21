988ef594bda73b1048f0c96cbd5278c6340282df
Anupam Chandra <anupamchanda2011@gmail.com>
2021-07-22 00:58:20 +0530

    array replaced with json structure

--- constant.js
+++ constant.js
@@ -1,61 +1,132 @@
-const mfUrl = " https://api.mfapi.in/mf/";
-const mfGroups = [
-	"largeCaps",
-	"midCaps"
+const mutualFunds = [
+	{
+		"type" : "largeCap",
+		"name" : "Large Cap Funds",
+		"data" : [
+			{
+				"name" : "Axis Bluechip Fund - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/axis-bluechip-fund-direct-plan-growth/MAA181",
+				"amfiLink" : "https://api.mfapi.in/mf/120465",
+				"threshold" : 50
+			},
+			{
+				"name" : "SBI Blue Chip Fund - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/sbi-blue-chip-fund-direct-plan-growth/MSB532",
+				"amfiLink" : "https://api.mfapi.in/mf/119598",
+				"threshold" : 57
+			},
+			{
+				"name" : "ICICI Prudential Bluechip Fund - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/icici-prudential-bluechip-fund-direct-plan-growth/MPI1134",
+				"amfiLink" : "https://api.mfapi.in/mf/120586",
+				"threshold" : 60
+			},
+			{
+				"name" : "Mirae Asset Large Cap Fund - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/mirae-asset-large-cap-fund-direct-plan-growth/MMA096",
+				"amfiLink" : "https://api.mfapi.in/mf/118825",
+				"threshold" : 75
+			},
+			{
+				"name" : "Aditya Birla Sun Life Frontline Equity Fund - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/aditya-birla-sun-life-frontline-equity-fund-direct-plan-growth/MBS813",
+				"amfiLink" : "https://api.mfapi.in/mf/119528",
+				"threshold" : 330
+			}
+		]
+	},
+	{
+		"type" : "midCap",
+		"name" : "Mid Cap Funds",
+		"data" : [
+			{
+				"name" : "HDFC Mid-Cap Opportunities Fund - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/hdfc-mid-cap-opportunities-fund-direct-plan-growth/MHD1161",
+				"amfiLink" : "https://api.mfapi.in/mf/118989",
+				"threshold" : 90
+			}
+		]
+	},
+	{
+		"type" : "focusedFund",
+		"name" : "Focused Funds",
+		"data" : [
+			{
+				"name" : "Axis Focused 25 Fund - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/axis-focused-25-fund-growth/MAA169",
+				"amfiLink" : "https://api.mfapi.in/mf/117560",
+				"threshold" : 40
+			},
+			{
+				"name" : "SBI Focused Equity Fund - Direct Plan - Growth",
+				"moneycontrolLink" : "https://www.moneycontrol.com/mutual-funds/nav/sbi-focused-equity-fund-direct-plan-growth/MSB536",
+				"amfiLink" : " https://api.mfapi.in/mf/119727",
+				"threshold" : 225
+			}
+		]
+	}
 ];
-const largeCaps = [
-	"Axis Bluechip Fund - Direct Plan - Growth",
-	"SBI Blue Chip Fund - Direct Plan - Growth",
-	"ICICI Prudential Bluechip Fund - Direct Plan - Growth",
-	"Mirae Asset Large Cap Fund - Direct Plan - Growth",
-	"Aditya Birla Sun Life Frontline Equity Fund - Direct Plan - Growth"
-];
-const largeCapLinks = [
-	"https://www.moneycontrol.com/mutual-funds/nav/axis-bluechip-fund-direct-plan-growth/MAA181",
- 	"https://www.moneycontrol.com/mutual-funds/nav/sbi-blue-chip-fund-direct-plan-growth/MSB532",
- 	"https://www.moneycontrol.com/mutual-funds/nav/icici-prudential-bluechip-fund-direct-plan-growth/MPI1134",
- 	"https://www.moneycontrol.com/mutual-funds/nav/mirae-asset-large-cap-fund-direct-plan-growth/MMA096",
- 	"https://www.moneycontrol.com/mutual-funds/nav/aditya-birla-sun-life-frontline-equity-fund-direct-plan-growth/MBS813"
-]
-const largeCapMFIds = [
-	"120465",
-	"119598",
-	"120586",
-	"118825",
-	"119528"
-];
-const largeCapNAVLimits = [
-	50,
-	57,
-	60,
-	75,
-	330
-];
-const midCaps = [
-	"HDFC Mid-Cap Opportunities Fund - Direct Plan - Growth"
-];
-const midCapLinks = [
- 	"https://www.moneycontrol.com/mutual-funds/nav/hdfc-mid-cap-opportunities-fund-direct-plan-growth/MHD1161"
-]
-const midCapMFIds = [
-	"118989"
-];
-const midCapNAVLimits = [
-	90
-];
-const focusedFunds = [
-	"Axis Focused 25 Fund - Growth",
-	"SBI Focused Equity Fund - Regular Plan - Growth"
-];
-const focusedFundsLinks = [
- 	"https://www.moneycontrol.com/mutual-funds/nav/axis-focused-25-fund-growth/MAA169",
- 	"https://www.moneycontrol.com/mutual-funds/nav/sbi-focused-equity-fund-regular-plan-growth/MSB059"
-]
-const focusedFundsMFIds = [
-	"117560",
-	"102756"
-];
-const focusedFundsNAVLimits = [
-	40,
-	210
-];
+
+
+
+
+// const mfGroups = [
+// 	"largeCaps",
+// 	"midCaps"
+// ];
+// const largeCaps = [
+// 	"Axis Bluechip Fund - Direct Plan - Growth",
+// 	"SBI Blue Chip Fund - Direct Plan - Growth",
+// 	"ICICI Prudential Bluechip Fund - Direct Plan - Growth",
+// 	"Mirae Asset Large Cap Fund - Direct Plan - Growth",
+// 	"Aditya Birla Sun Life Frontline Equity Fund - Direct Plan - Growth"
+// ];
+// const largeCapLinks = [
+// 	"https://www.moneycontrol.com/mutual-funds/nav/axis-bluechip-fund-direct-plan-growth/MAA181",
+//  	"https://www.moneycontrol.com/mutual-funds/nav/sbi-blue-chip-fund-direct-plan-growth/MSB532",
+//  	"https://www.moneycontrol.com/mutual-funds/nav/icici-prudential-bluechip-fund-direct-plan-growth/MPI1134",
+//  	"https://www.moneycontrol.com/mutual-funds/nav/mirae-asset-large-cap-fund-direct-plan-growth/MMA096",
+//  	"https://www.moneycontrol.com/mutual-funds/nav/aditya-birla-sun-life-frontline-equity-fund-direct-plan-growth/MBS813"
+// ]
+// const largeCapMFIds = [
+// 	"120465",
+// 	"119598",
+// 	"120586",
+// 	"118825",
+// 	"119528"
+// ];
+// const largeCapNAVLimits = [
+// 	50,
+// 	57,
+// 	60,
+// 	75,
+// 	330
+// ];
+// const midCaps = [
+// 	"HDFC Mid-Cap Opportunities Fund - Direct Plan - Growth"
+// ];
+// const midCapLinks = [
+//  	"https://www.moneycontrol.com/mutual-funds/nav/hdfc-mid-cap-opportunities-fund-direct-plan-growth/MHD1161"
+// ]
+// const midCapMFIds = [
+// 	"118989"
+// ];
+// const midCapNAVLimits = [
+// 	90
+// ];
+// const focusedFunds = [
+// 	"Axis Focused 25 Fund - Growth",
+// 	"SBI Focused Equity Fund - Regular Plan - Growth"
+// ];
+// const focusedFundsLinks = [
+//  	"https://www.moneycontrol.com/mutual-funds/nav/axis-focused-25-fund-growth/MAA169",
+//  	"https://www.moneycontrol.com/mutual-funds/nav/sbi-focused-equity-fund-regular-plan-growth/MSB059"
+// ]
+// const focusedFundsMFIds = [
+// 	"117560",
+// 	"102756"
+// ];
+// const focusedFundsNAVLimits = [
+// 	40,
+// 	210
+// ];
--- utility.js
+++ utility.js
@@ -5,20 +5,20 @@ function httpGet(theUrl){
     return JSON.parse(xmlHttp.responseText);
 }
 
-function drawTable(table, mfNames, mfUrl, links, navLimits, mfIds, divId){
+function drawTableForFund(table, data, divId){
 	var i;
-	for(i = 0; i < mfNames.length; i++){
-		var mfDetails = httpGet(mfUrl + mfIds[i]);
+	for(i = 0; i < data.length; i++){
+		var mfDetails = httpGet(data[i].amfiLink);
 		var currentNAV = parseInt(mfDetails.data[0].nav);
-		if(currentNAV >= navLimits[i]){
+		if(currentNAV >= data[i].threshold){
 			table = table + "<tr><td>";
 		}
 		else{
 			table = table + "<tr style='background-color:#e6ffe6;'><td>";
 		}
-		table = table + mfNames[i] + "</td><td>";
+		table = table + data[i].name + "</td><td>";
 		table = table + mfDetails.data[0].nav + "</td><td>";
-		table = table + navLimits[i] + "</td>";
+		table = table + data[i].threshold + "</td>";
 		var diff = (mfDetails.data[0].nav - mfDetails.data[1].nav).toFixed(2);
 		if(diff < 0){
 			table = table + "<td style='color:red'>";
@@ -27,8 +27,28 @@ function drawTable(table, mfNames, mfUrl, links, navLimits, mfIds, divId){
 			table = table + "<td style='color:green'>";
 		}
 		table = table + diff + "</td><td><a href='";
-		table = table + links[i] + "'>Links</a></td></tr>";
+		table = table + data[i].moneycontrolLink + "'>Links</a></td></tr>";
 	}
 	table = table + "</table>";
 	document.getElementById(divId).innerHTML = table;
+}
+
+function drawPage(mutualFunds){
+	var i;
+	var table = "";
+	for(i = 0; i < mutualFunds.length; i++){
+		table = table + "<table id='funds'> <tr><th>";
+		table = table + mutualFunds[i].name + "</th><th>NAV Value</th><th>NAV Threshold</th><th>Difference</th><th>Details</th></tr>"
+		drawTableForFund(table, mutualFunds[i].data, mutualFunds[i].type);
+		table = "";
+	}
+}
+
+function drawTable(mutualFunds){
+	var i;
+	var table = "";
+		for(i = 0; i < mutualFunds.length; i++){
+		table = table + "<div id='" + mutualFunds[i].type + "'></div></br>";
+	}
+	document.getElementById("mutualFunds").innerHTML = table;
 }
